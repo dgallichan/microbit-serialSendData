5e1f439206703980bd50f8c786d0fe3b37fdfcf8
Daniel Gallichan <dgallichan@yahoo.com>
2024-06-03 09:35:22 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,19 @@
 function writeAccMag () {
-    serial.writeLine("" + control.micros() + delimiter + input.acceleration(Dimension.X) + delimiter + input.acceleration(Dimension.Y) + delimiter + input.acceleration(Dimension.Z) + delimiter + input.magneticForce(Dimension.X) + delimiter + input.magneticForce(Dimension.Y) + delimiter + input.magneticForce(Dimension.Z))
+    serial.writeLine("" + thisLine + input.acceleration(Dimension.X) + delimiter + input.acceleration(Dimension.Y) + delimiter + input.acceleration(Dimension.Z) + delimiter + input.magneticForce(Dimension.X) + delimiter + input.magneticForce(Dimension.Y) + delimiter + input.magneticForce(Dimension.Z))
 }
 function writeMagOnly () {
-    serial.writeLine("" + control.micros() + delimiter + input.magneticForce(Dimension.X) + delimiter + input.magneticForce(Dimension.Y) + delimiter + input.magneticForce(Dimension.Z))
+    serial.writeLine("" + thisLine + input.magneticForce(Dimension.X) + delimiter + input.magneticForce(Dimension.Y) + delimiter + input.magneticForce(Dimension.Z))
 }
 function writeAccOnly () {
-    serial.writeLine("" + control.micros() + delimiter + input.acceleration(Dimension.X) + delimiter + input.acceleration(Dimension.Y) + delimiter + input.acceleration(Dimension.Z))
+    serial.writeLine("" + thisLine + input.acceleration(Dimension.X) + delimiter + input.acceleration(Dimension.Y) + delimiter + input.acceleration(Dimension.Z))
 }
 function writeP2 () {
-    serial.writeLine("" + control.micros() + delimiter + pins.analogReadPin(AnalogPin.P2))
+    serial.writeLine("" + (pins.analogReadPin(AnalogPin.P0)))
 }
 function writeTimeOnly () {
     serial.writeLine("" + (control.micros()))
 }
+let thisLine = ""
 let delimiter = ""
 let loop_time = 0
 basic.showLeds(`
@@ -23,10 +24,16 @@ basic.showLeds(`
     # # # # #
     `)
 delimiter = ","
+let includeTime = 0
 control.inBackground(function () {
     while (true) {
-        writeP2()
-        while (control.micros() - loop_time < 5000) {
+        if (includeTime == 1) {
+            thisLine = "" + control.micros() + delimiter
+        } else {
+            thisLine = ""
+        }
+        writeAccOnly()
+        while (control.micros() - loop_time < 25000) {
         	
         }
         loop_time = control.micros()
